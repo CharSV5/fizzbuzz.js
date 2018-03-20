bc9c200bbf7c8274f237f46b571c28efbb26f0d7
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-20 15:21:10 +0000

    tests for divisble by 5 and 3 passing

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,34 @@
+describe('Fizzbuzz', function() {
+  var fizzbuzz;
+
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz()
+  });
+
+  describe('knows when a number is', function() {
+    it('divisible by 3', function() {
+
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe('know when a number is NOT', function() {
+    it('divisible by 3', function() {
+
+      expect(fizzbuzz.isDivisibleByThree(2)).toBe(false);
+    });
+  })
+
+  describe('knows when a number is', function() {
+    it('divisible by 5', function () {
+
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('know when a number is NOT', function() {
+    it('divisible by five', function() {
+      expect(fizzbuzz.isDivisibleByFive(9)).toBe(false);
+    });
+  });
+});
